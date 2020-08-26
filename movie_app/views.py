97638822cbd8af792d6156d20868e4a79423362c
MrBreakIT <johnpike1022@gmail.com>
2020-08-26 18:13:45 -0500

    First commit

---
+++ movie_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Movie
+
+def index(request):
+    context = {
+        "all_the_movies": Movie.objects.all()
+    }
+return render (request, "index.html", context)
+# Create your views here.
